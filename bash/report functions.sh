cfb91b852af403bff634f95ee3cc6e1e1e5b1430
kelseysbrose <31860826+kelseysbrose@users.noreply.github.com>
2023-06-30 21:45:57 -0400

    Update report functions.sh

--- bash/report functions.sh
+++ bash/report functions.sh
@@ -1,1 +1,9 @@
 Kelsey Rose 
+
+# Function for saving timestame error message 
+function errormessage() {
+  local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    local error_message="$1"
+    echo "[$timestamp] $error_message" >> /var/log/systeminfo.log
+    echo "$error_message" >&2
+}
