50c86f4df8af42bb02f153f87c4a6379f3a75141
kelseysbrose <31860826+kelseysbrose@users.noreply.github.com>
2023-08-10 17:17:43 -0400

    morechangestoit

--- Powershell/Sysinfo.ps1
+++ Powershell/Sysinfo.ps1
@@ -14,15 +14,6 @@ $systemInfo = [PSCustomObject]@{
 	Domain = $ComputerHardware.Domain
   }
 	
-# Replacing the empty values with N/A
-
-$systemInfo | ForEach-Object {
-	$_.PSObject.Properties | ForEach-Object {
-		if ($_.Value -eq "") {
-			$_.Value = "N/A"
-	}
-    }
-}
 
 
 # Make it have a title 
@@ -40,15 +31,7 @@ $OperatingSystem = Get-CIMInstance -Classname Win32_OperatingSystem | Select-Obj
 Write-Host "Operating System Information"
 $OperatingSystem |Format-Table -Autosize
 
-# Replacing the empty values with N/A
 
-$OperatingSystem | ForEach-Object {
-	$_.PSObject.Properties | ForEach-Object {
-		if ($_.Value -eq " ") {
-			$_.Value = "N/A"
-	}
-    }
-}
 
 
 # Then you want to get the Processor Information: 
@@ -58,6 +41,7 @@ $Processor = Get-CimInstance -ClassName win32_processor | Select-Object -Propert
 Write-Host "Processor Information"
 $Processor | Format-Table -Autosize
 
+
 $Processor | ForEach-Object {
 	$_.PSObject.Properties | ForEach-Object {
 		if ($_.Value -eq "Null") {
@@ -65,4 +49,107 @@ $Processor | ForEach-Object {
 	}
     }
 }
+
+
+# For the Disk Drives 
+
+Write-Host "Disk Drive Information" 
+
+
+$diskdrives = Get-CIMInstance CIM_diskdrive
+$diskInfoList = @()
+
+  foreach ($disk in $diskdrives) {
+      $partitions = $disk | get-cimassociatedinstance -resultclassname CIM_diskpartition
+      foreach ($partition in $partitions) {
+            $logicaldisks = $partition | get-cimassociatedinstance -resultclassname CIM_logicaldisk
+            foreach ($logicaldisk in $logicaldisks) {
+		$usedSpacePercentage = ($logicaldisk.size - $logicaldisk.freespace) / $logicaldisk.size * 100
+
+                 $diskInfo = [PSCustomObject]@{
+			Manufacturer = $disk.Manufacturer
+			Location = $partition.deviceid
+			Drive = $logicaldisk.deviceid
+			"Size(GB)" = [int]($logicaldisk.size / 1GB)
+			"UsedSpace(%)" = [math]::Round($usedSpacePercentage, 2)
+	}
+		$diskinfolist += $diskinfo
+                                                               
+           }
+      }
+  }
+          
+
+$diskinfolist
+
+
+## Now it's the RAM's turn 
+
+Write-Host "Ram Information"
+
+$ramInfoList = @()
+$ramTotal = 0
+$physicalmemory = Get-WmiObject win32_physicalmemory
+	foreach ($ram in $physicalmemory) {
+	$ramInfo = [PSCustomObject]@{
+		vendor = $ram.Manufacturer
+		Description = $ram.Description
+		Size = [int]($ram.Capacity / 1gb)
+		BankAndSlot = "Bank $ram.Banklabel, Slot $ram.DeviceLocator"
+	}
+		$ramInfoList += $ramInfo
+ }
+
+$ramInfoList | Format-Table Vendor,Description, Size, BankAndSlot -Autosize
+
+# Total Installed Ram 
+$ramTotalGB = [int]($ramTotal / 1gb)
+Write-Host "Total RAM Installed: $ramTotalGB GB"
+
+
+
+Write-Host " Network Configuration"
+
+# This script will make a table for the ipconfig information that is enabled. 
+
+$adapterConfigurations = Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true }
+
+# This gathered the information that will be displayed with the table below in the differnt headers is the information comes back enabled or true
+
+$ipConfigReport = $adapterConfigurations | ForEach-Object {
+	$dnsServers = $_.DNSServerSearchOrder -join ','
+	$ipAddress = $_.IPAddress -join ','
+	$subnetMasks = $_.IPSubnet -join ','
+
+# Making the custom object table below for the headers specified. 
+
+	[PSCustomObject]@{
+	AdapterDescription =$_.Description
+	Index = $_.Index
+	IPAdress = $ipaddress
+	SubnetMask = $subnetMasks
+	DNSDomain = $_.DNSDomain
+	DNSServer = $dnsServers
+    }
+}
+
+# This command is to format the table and autosize it
+
+$ipConfigReport | Format-Table -Autosize
+
+# Lastly, Video card Info 
+
+$VideoInfoList = @()
+$VideoControllers = Get-WmiObject win32_videoController
+foreach ($video in $videoControllers) {
+	$resolution = "$($video.CurrentHorizontalResolution) x $($video.CurrentVerticalResolution)"
+	$videoInfo = [PSCustomObject] {
+	Vendor = $video.Processor
+	Description = $video.Description
+	Resolution = $resolution
+	}
+$VideoInfoList += $videoinfo
+}
+
+$videoInfoList | Format-Table Vendor, Description, Resolution -Autosize
 
